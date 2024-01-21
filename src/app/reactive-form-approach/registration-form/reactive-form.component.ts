61bd896602c8b3912c0329d74de579cb84291b37
NaeemSardar <naeemsardar.cs@gmail.com>
2024-01-21 23:54:47 +0500

    again worked on these files

--- src/app/reactive-form-approach/registration-form/reactive-form.component.ts
+++ src/app/reactive-form-approach/registration-form/reactive-form.component.ts
@@ -1,49 +1,94 @@
 import { Component } from '@angular/core';
-import { FormGroup,FormControl,FormBuilder, NgForm, Validators} from '@angular/forms';
+import { FormGroup,FormControl,FormBuilder, NgForm, Validators, AbstractControl, FormArray} from '@angular/forms';
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
   styleUrl: './reactive-form.component.css'
 })
 export class ReactiveFormComponent{
+ 
+  provinceArray: string[] = ['Punjab','Sind','KPK',];
+  
+  constructor (private fb:FormBuilder){}
 
-  myForm:FormGroup; 
-
-  firstname :string = "";
-
-constructor (formbuilder:FormBuilder)
-{
-    this.myForm = formbuilder.group({
-      txtfirstname: ['',Validators.required ],
+    myForm = this.fb.group({
+      txtfirstname: ['',
+      [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(8)],
+      ],
       txtsecondname:['',Validators.required ],
       dob:['',Validators.required ],
       rdgender:['',Validators.required ],
       slcourse:['',Validators.required ],
-      Punjab:['',Validators.required ],
-      Sind:['',Validators.required ],
-      KPK:['',Validators.required ],
+      province:this.fb.array([]),
       txtaddress:['',Validators.required ],
       txtnumber:['',Validators.required ],
+     txtemail:['',
+     [
+      Validators.required,
+      Validators.email ]
+    ],
+    accept:['',Validators.requiredTrue ],
     });
-}
-ngOnInit()
+   
+  submmited = false;
+
+
+ngOnInit():void
 {
   
 }
-submitForm(form:any)
+get f(): { [key: string]: AbstractControl } {
+  return this.myForm.controls;
+}
+
+onChange(e:any)
 {
+  console.log(e.target.value,e.target.checked)
+  const checkArray = this.myForm.get("province") as FormArray
+  if (e.target.checked) 
+  {
+    checkArray.push(new FormControl(e.target.value));
+  } 
+  else 
+  {
+    let i: number = 0;
+    checkArray.controls.forEach((item) => {
+      if (item.value == e.target.value) {
+        checkArray.removeAt(i);
+        return;
+      }
+      i++;
+    });
+  }
+}
+
+
+submitForm()
+{
+  this.submmited = true;
+
+  if(this.myForm.invalid)
+  {
+    console.log(this.myForm.value)
+    return;
+  }
+  else
+  {
+    console.log(JSON.stringify(this.myForm.value));
+  }
   // this.firstname =  this.myForm.get('txtfirstname')?.value;
   // console.log(form.controls.txtfirstname.value);
  
-  console.log(form.controls.value);
+  // console.log(form.controls.txtaddress.value);
 
-  // console.log(this.myForm.value);
-
-  
 }
 
 resetForm()
 {
+  this.submmited = false;
   this.myForm.reset();
 }
 }
